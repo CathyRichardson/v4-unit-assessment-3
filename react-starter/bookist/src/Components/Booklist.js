426f6b46d7c1eea561ec2083e2c8752c4b97e710
Cathy Richardson <cathyanne08@gmail.com>
2021-06-22 22:38:20 -0600

    updated the map function

--- react-starter/bookist/src/Components/Booklist.js
+++ react-starter/bookist/src/Components/Booklist.js
@@ -2,21 +2,21 @@ import App from "../App";
 
 function BookList(props) {
 
-    function book(book, index) {
+    //callback function for map
+    function book(element, index) {
         return (
             <div className="book" key={index}>
-                <img src={book.img} />
-                <h3>{book.title}</h3>
-                <h4>{book.author}</h4>
+                <img src={element.img} />
+                <h3>{element.title}</h3>
+                <h4>{element.author}</h4>
             </div>
         )
     }
 
     return (
         < div className="bookMap">
-            {props.books.map((e, i) => book(e, i))}
+            {props.books.map(book)}
         </div >
-
     )
 }
 
